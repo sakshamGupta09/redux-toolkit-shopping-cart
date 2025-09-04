05180848948eaea869f25a76d33bee519e3a73f9
Saksham Gupta <saksham.09.gupta@gmail.com>
2025-09-04 05:52:49 +0530

    Render different logos based on screen sizes

--- src/app/components/layout/Logo/Logo.tsx
+++ src/app/components/layout/Logo/Logo.tsx
@@ -2,13 +2,18 @@ import styles from "./Logo.module.css";
 
 export default function Logo() {
   return (
-    <img
-      className={styles.logo}
-      src="/logo.avif"
-      width="271"
-      height="125"
-      loading="lazy"
-      alt="Logo"
-    />
+    <picture title="Shopping App Logo">
+      <source
+        type="image/avif"
+        media="(min-width:780px)"
+        srcSet="logos/logo-lg.avif"
+      />
+      <source
+        media="(max-width:465px)"
+        srcSet="logos/logo-sm.avif"
+        type="image/avif"
+      />
+      <img src="logos/logo-sm.avif" alt="Logo" className={styles.logo} />
+    </picture>
   );
 }
