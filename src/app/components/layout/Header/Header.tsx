031a99fc6acd6fb06059fa2fb13da7d89722ac75
Saksham Gupta <saksham.09.gupta@gmail.com>
2025-09-04 07:04:56 +0530

    Responsive header on desktop

--- src/app/components/layout/Header/Header.tsx
+++ src/app/components/layout/Header/Header.tsx
@@ -5,6 +5,9 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <Logo />
+      <div className={styles.searchBar}>Search Bar</div>
+      <p className="ml-auto">Cart</p>
+      <p>Login</p>
     </header>
   );
 }
--- src/app/components/layout/Logo/Logo.tsx
+++ src/app/components/layout/Logo/Logo.tsx
@@ -1,23 +1,26 @@
+import { Link } from "react-router";
 import styles from "./Logo.module.css";
 
 export default function Logo() {
   return (
-    <picture title="Shopping App Logo">
-      <source
-        type="image/avif"
-        media="(width > 800px)"
-        srcSet="https://www.ugaoo.com/cdn/shop/files/logo.png?height=100&v=1738571809"
-      />
-      <source
-        type="image/avif"
-        media="(width <= 800px)"
-        srcSet="https://www.ugaoo.com/cdn/shop/files/logo.png?height=80&v=1738571809"
-      />
-      <img
-        src="https://www.ugaoo.com/cdn/shop/files/logo.png?height=80&v=1738571809"
-        alt="Logo"
-        className={styles.logo}
-      />
-    </picture>
+    <Link to="/">
+      <picture title="Shopping App Logo">
+        <source
+          type="image/avif"
+          media="(width > 800px)"
+          srcSet="https://www.ugaoo.com/cdn/shop/files/logo.png?height=100&v=1738571809"
+        />
+        <source
+          type="image/avif"
+          media="(width <= 800px)"
+          srcSet="https://www.ugaoo.com/cdn/shop/files/logo.png?height=80&v=1738571809"
+        />
+        <img
+          src="https://www.ugaoo.com/cdn/shop/files/logo.png?height=80&v=1738571809"
+          alt="Logo"
+          className={styles.logo}
+        />
+      </picture>
+    </Link>
   );
 }
