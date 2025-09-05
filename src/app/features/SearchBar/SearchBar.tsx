1685c15109b7f8b50761d7260db433debc5b1d88
Saksham Gupta <saksham.09.gupta@gmail.com>
2025-09-05 07:31:53 +0530

    Responsive search bar component

--- src/app/features/SearchBar/SearchBar.tsx
+++ src/app/features/SearchBar/SearchBar.tsx
@@ -1,3 +1,12 @@
+import styles from "./SearchBar.module.css";
+
 export default function SearchBar() {
-  return <div className="searchBar">Search BAR</div>;
+  return (
+    <form className={styles.searchContainer}>
+      <label htmlFor="searchField" className="d-flex">
+        <img src="search.svg" alt="Search products icon" />
+      </label>
+      <input type="search" id="searchField" placeholder="Search for products, brands and more" />
+    </form>
+  );
 }
