75507c6ceb673e9ad92386d0a391687de6906c1e
Saksham Gupta <saksham.09.gupta@gmail.com>
2025-09-12 04:51:27 +0530

    Use absolute paths

---
+++ src/app/store.ts
@@ -0,0 +1,21 @@
+import {
+  configureStore,
+  type Action,
+  type ThunkAction,
+} from "@reduxjs/toolkit";
+
+import categoriesReducer from "@/features/categories/slice";
+
+export const store = configureStore({
+  reducer: {
+    categories: categoriesReducer,
+  },
+});
+
+export type AppStore = typeof store;
+
+export type AppDispatch = typeof store.dispatch;
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppThunk = ThunkAction<void, RootState, unknown, Action>;
