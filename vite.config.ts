bcd270884a2acdc832eefeae8d2607eec9005262
Saksham Gupta <saksham.09.gupta@gmail.com>
2025-09-12 08:09:02 +0530

    Fix failing builds

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,14 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@routes": path.resolve(__dirname, "src/routes"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+      "@models": path.resolve(__dirname, "src/models"),
+      "@api": path.resolve(__dirname, "src/api"),
+      "@app": path.resolve(__dirname, "src/app"),
     },
   },
 });
