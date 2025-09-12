55509d61bf68ddd5a709af9228cde1e8509882de
Saksham Gupta <saksham.09.gupta@gmail.com>
2025-09-12 10:44:05 +0530

    Add more products

--- src/features/categories/constants/categories.ts
+++ src/features/categories/constants/categories.ts
@@ -36,4 +36,49 @@ export const CATEGORIES: IProductCategory[] = [
     name: "Home decorations",
     img: "https://rukminim2.flixcart.com/fk-p-flap/128/128/image/433b0f4740e83c8e.jpg?q=100",
   },
+  {
+    id: "kitchen-accessories",
+    name: "Kitchen Accessories",
+    img: "https://rukminim2.flixcart.com/image/128/128/xif0q/container/x/2/y/6-fridge-storage-containers-fridge-organizer-with-removable-original-imahfjz8yrudzhzt.jpeg?q=100",
+  },
+  {
+    id: "mens-shirts",
+    name: "Mens Shirts",
+    img: "https://rukminim2.flixcart.com/image/128/128/xif0q/shirt/v/9/f/xxl-deu020-deneeja-original-imah9wmc5bkhgagn.jpeg?q=100",
+  },
+  {
+    id: "mens-shoes",
+    name: "Mens Shoes",
+    img: "https://rukminim2.flixcart.com/image/128/128/xif0q/shoe/x/p/k/7-bis-snk-eva-851-blu-7-bruton-blue-original-imahbch75zuzxgr8.jpeg?q=100",
+  },
+  {
+    id: "mobile-accessories",
+    name: "Mobile Accessories",
+    img: "https://rukminim2.flixcart.com/image/128/128/xif0q/battery-charger/l/p/d/33w-power-adapter-with-usb-to-type-c-cable-roarx-original-imah7uvqtj5fbqdx.jpeg?q=100",
+  },
+  {
+    id: "skin-care",
+    name: "Skin Care",
+    img: "https://rukminim2.flixcart.com/image/128/128/xif0q/moisturizer-cream/h/2/p/220-pure-aloe-vera-gel-220-gram-ideal-for-skin-care-face-acne-original-imagqq564f95bfdb.jpeg?q=100",
+  },
+  {
+    id: "sunglasses",
+    name: "Sunglasses",
+    img: "https://rukminim2.flixcart.com/image/128/128/xif0q/sunglass/3/2/h/m-pnt-1m-accentz-original-imahcvzmxa4fgeuc.jpeg?q=100",
+  },
+  {
+    id: "tops",
+    name: "Tops",
+    img: "https://rukminim2.flixcart.com/image/128/128/xif0q/top/r/a/l/s-gr7054-white-harpa-original-imagmfqdnsdjgpg8.jpeg?q=100",
+  },
+  {
+    id: "womens-watches",
+    name: "Watches",
+    img: "https://rukminim2.flixcart.com/image/128/128/xif0q/watch/m/k/m/1-db-brown-laksh07-risthey-women-original-imahffpzxhy8byfs.jpeg?q=100",
+  },
+  {
+    id: "womens-jewellery",
+    name: "Jewellery",
+    img: "https://rukminim2.flixcart.com/image/128/128/xif0q/jewellery-set/6/8/0/na-na-1-r5379-atasi-international-original-imagr77hmpvytyes.jpeg?q=100",
+  },
 ];
